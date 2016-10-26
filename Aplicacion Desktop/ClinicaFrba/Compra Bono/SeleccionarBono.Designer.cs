91f524dc42b64b0e1fab358f747e9344989f9b2d
GuibuA <guibuadrian@gmail.com>
2016-10-26 07:31:33 -0300

    Seleccion de bonos

--- Aplicacion Desktop/ClinicaFrba/Compra Bono/SeleccionarBono.Designer.cs
+++ Aplicacion Desktop/ClinicaFrba/Compra Bono/SeleccionarBono.Designer.cs
@@ -29,6 +29,8 @@
         private void InitializeComponent()
         {
             this.buttonVolver = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
             // 
             // buttonVolver
@@ -42,20 +44,42 @@
             this.buttonVolver.UseVisualStyleBackColor = true;
             this.buttonVolver.Click += new System.EventHandler(this.buttonVolver_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(106, 13);
+            this.label1.TabIndex = 50;
+            this.label1.Text = "Seleccione un Bono:";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(125, 33);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 51;
+            // 
             // SeleccionarBono
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.buttonVolver);
             this.Name = "SeleccionarBono";
             this.Text = "SeleccionarBono";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button buttonVolver;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
     }
 }
