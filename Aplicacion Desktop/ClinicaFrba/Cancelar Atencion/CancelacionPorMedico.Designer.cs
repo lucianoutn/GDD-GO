9117e45deb02243066de6d1df7a29a510951cb34
GuibuA <guibuadrian@gmail.com>
2016-11-07 01:46:28 -0300

    Dar de baja un afiliado cancela todos sus turnos. Sql andate a la p* q te pario y Moscuzza tambien. Aguante el pity loco, aguante intoxicado

--- Aplicacion Desktop/ClinicaFrba/Cancelar Atencion/CancelacionPorMedico.Designer.cs
+++ Aplicacion Desktop/ClinicaFrba/Cancelar Atencion/CancelacionPorMedico.Designer.cs
@@ -30,13 +30,20 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CancelacionPorMedico));
             this.buttonVolver = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dateTimePickerFin = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePickerIni = new System.Windows.Forms.DateTimePicker();
+            this.buttonSeleccionar = new System.Windows.Forms.Button();
+            this.textBoxMotivo = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // buttonVolver
             // 
             this.buttonVolver.Cursor = System.Windows.Forms.Cursors.Hand;
             this.buttonVolver.ForeColor = System.Drawing.Color.Maroon;
-            this.buttonVolver.Location = new System.Drawing.Point(40, 213);
+            this.buttonVolver.Location = new System.Drawing.Point(76, 188);
             this.buttonVolver.Name = "buttonVolver";
             this.buttonVolver.Size = new System.Drawing.Size(75, 23);
             this.buttonVolver.TabIndex = 0;
@@ -44,22 +51,95 @@
             this.buttonVolver.UseVisualStyleBackColor = true;
             this.buttonVolver.Click += new System.EventHandler(this.buttonVolver_Click);
             // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(80, 141);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(71, 13);
+            this.label4.TabIndex = 19;
+            this.label4.Text = "Fecha Hasta:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(77, 88);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(74, 13);
+            this.label3.TabIndex = 18;
+            this.label3.Text = "Fecha Desde:";
+            // 
+            // dateTimePickerFin
+            // 
+            this.dateTimePickerFin.Location = new System.Drawing.Point(225, 135);
+            this.dateTimePickerFin.Name = "dateTimePickerFin";
+            this.dateTimePickerFin.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePickerFin.TabIndex = 17;
+            // 
+            // dateTimePickerIni
+            // 
+            this.dateTimePickerIni.Location = new System.Drawing.Point(225, 82);
+            this.dateTimePickerIni.Name = "dateTimePickerIni";
+            this.dateTimePickerIni.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePickerIni.TabIndex = 16;
+            // 
+            // buttonSeleccionar
+            // 
+            this.buttonSeleccionar.Location = new System.Drawing.Point(350, 188);
+            this.buttonSeleccionar.Name = "buttonSeleccionar";
+            this.buttonSeleccionar.Size = new System.Drawing.Size(75, 23);
+            this.buttonSeleccionar.TabIndex = 20;
+            this.buttonSeleccionar.Text = "Seleccionar";
+            this.buttonSeleccionar.UseVisualStyleBackColor = true;
+            this.buttonSeleccionar.Click += new System.EventHandler(this.buttonSeleccionar_Click);
+            // 
+            // textBoxMotivo
+            // 
+            this.textBoxMotivo.Location = new System.Drawing.Point(225, 33);
+            this.textBoxMotivo.Name = "textBoxMotivo";
+            this.textBoxMotivo.Size = new System.Drawing.Size(100, 20);
+            this.textBoxMotivo.TabIndex = 21;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(109, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 13);
+            this.label1.TabIndex = 22;
+            this.label1.Text = "Motivo:";
+            // 
             // CancelacionPorMedico
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(525, 245);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxMotivo);
+            this.Controls.Add(this.buttonSeleccionar);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.dateTimePickerFin);
+            this.Controls.Add(this.dateTimePickerIni);
             this.Controls.Add(this.buttonVolver);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "CancelacionPorMedico";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "CancelacionPorMedico";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button buttonVolver;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DateTimePicker dateTimePickerFin;
+        private System.Windows.Forms.DateTimePicker dateTimePickerIni;
+        private System.Windows.Forms.Button buttonSeleccionar;
+        private System.Windows.Forms.TextBox textBoxMotivo;
+        private System.Windows.Forms.Label label1;
     }
 }
--- Aplicacion Desktop/ClinicaFrba/Cancelar Atencion/CancelacionPorMedico.cs
+++ Aplicacion Desktop/ClinicaFrba/Cancelar Atencion/CancelacionPorMedico.cs
@@ -6,25 +6,46 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using ClinicaFrba.DataBase.Conexion;
 
 namespace ClinicaFrba.Cancelar_Atencion
 {
     public partial class CancelacionPorMedico : Form
     {
         Form unMenu;
-        String profesional;
+        String unProfesional;
+        Turno_DAO turno_dao;
 
         public CancelacionPorMedico(Form menu, String id_profesional)
         {
+            turno_dao = new Turno_DAO();
             InitializeComponent();
             unMenu = menu;
-            profesional = id_profesional;
+            unProfesional = id_profesional;
         }
 
         private void buttonVolver_Click(object sender, EventArgs e)
         {
             unMenu.Show();
             this.Close();
+        }
+
+        private void buttonSeleccionar_Click(object sender, EventArgs e)
+        {
+            if (!validar())
+            {
+                MessageBox.Show("No valido");
+            }
+            else
+            {
+                turno_dao.cancelarTurnoProfesional(textBoxMotivo.Text, unProfesional, dateTimePickerIni.Value, dateTimePickerFin.Value);
+                MessageBox.Show("Turnos cancelados");
+            }
+        }
+
+        private bool validar()
+        {
+            return dateTimePickerFin.Value>dateTimePickerIni.Value;
         }
     }
 }
