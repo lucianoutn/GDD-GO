87863af433e847556c3edadf96210b26d69d68db
lucianoutn <luciano.utn@gmail.com>
2016-11-20 16:12:48 -0300

    estilo

--- Aplicacion Desktop/ClinicaFrba/Compra Bono/CompraBono.Designer.cs
+++ Aplicacion Desktop/ClinicaFrba/Compra Bono/CompraBono.Designer.cs
@@ -33,6 +33,8 @@
             this.textBoxIdAfiliado = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.buttonSiguiente = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // buttonVolver
@@ -74,19 +76,33 @@
             this.buttonSiguiente.UseVisualStyleBackColor = true;
             this.buttonSiguiente.Click += new System.EventHandler(this.buttonSiguiente_Click);
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::ClinicaFrba.Properties.Resources.farmacia2;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 59);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(257, 203);
+            this.pictureBox1.TabIndex = 53;
+            this.pictureBox1.TabStop = false;
+            // 
             // CompraBono
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
             this.ClientSize = new System.Drawing.Size(284, 262);
             this.Controls.Add(this.buttonSiguiente);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBoxIdAfiliado);
             this.Controls.Add(this.buttonVolver);
+            this.Controls.Add(this.pictureBox1);
+            this.DoubleBuffered = true;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "CompraBono";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Comprar Bono";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -98,5 +114,6 @@
         private System.Windows.Forms.TextBox textBoxIdAfiliado;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button buttonSiguiente;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
