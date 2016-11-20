e77b87a6dc05c56b293a5b6bd0a6b8cd1cf4e7d3
GuibuA <guibuadrian@gmail.com>
2016-11-20 20:45:35 -0300

    Busqueda de afiliado en Compra de Bono

--- Aplicacion Desktop/ClinicaFrba/Compra Bono/CompraBono.cs
+++ Aplicacion Desktop/ClinicaFrba/Compra Bono/CompraBono.cs
@@ -16,6 +16,7 @@ namespace ClinicaFrba.Compra_Bono
         Menu unMenu;
         ABM_usuario_DAO abm_usuario;
         PlanMedico_DAO plan_medico_dao;
+        List<string> lista_usuarios_afiliados = new List<string>();
 
         public CompraBono(Menu menu)
         {
@@ -26,34 +27,79 @@ namespace ClinicaFrba.Compra_Bono
             plan_medico_dao = new PlanMedico_DAO();
         }
 
-        private void buttonVolver_Click(object sender, EventArgs e)
+        private void buttonBuscar_Click(object sender, EventArgs e)
+        {
+            dataGridViewResultados.Rows.Clear();
+            dataGridViewResultados.Refresh();
+
+            String desc_nombre = textBoxNombre.Text;
+            String desc_apellido = textBoxApellido.Text;
+            String desc_dni = textBoxDni.Text;
+            String desc_id = textBoxId.Text;
+
+            if (string.IsNullOrWhiteSpace(textBoxId.Text))
+            {
+                lista_usuarios_afiliados = abm_usuario.get_id_afiliado_multiple(desc_nombre, desc_apellido, desc_dni);
+
+
+                for (int i = 0; i < lista_usuarios_afiliados.Count; i++)
+                {
+                    desc_id = lista_usuarios_afiliados[i];
+
+                    dataGridViewResultados.Rows.Add(desc_id,
+                                                    abm_usuario.get_nombre(desc_id).ToString(),
+                                                    abm_usuario.get_apellido(desc_id).ToString(),
+                                                    abm_usuario.get_dni(desc_id).ToString(),
+                                                    abm_usuario.get_id_usuario(desc_id).ToString());
+                }
+            }
+            else
+            {
+                int dni = abm_usuario.get_dni(desc_id);
+
+                if (dni != 0)
+                {
+                    dataGridViewResultados.Rows.Add(desc_id,
+                                                    abm_usuario.get_nombre(desc_id).ToString(),
+                                                    abm_usuario.get_apellido(desc_id).ToString(),
+                                                    dni.ToString(),
+                                                    abm_usuario.get_id_usuario(desc_id).ToString());
+                }
+
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
         {
             unMenu.Show();
             this.Close();
+
         }
 
-        private void buttonSiguiente_Click(object sender, EventArgs e)
+        private void buttonEliminar_Click(object sender, EventArgs e)
         {
-            if (!string.IsNullOrWhiteSpace(textBoxIdAfiliado.Text))
+            DataGridViewRow fila = dataGridViewResultados.SelectedRows[0];
+            try
             {
-                if (!string.IsNullOrWhiteSpace(abm_usuario.get_nombre(textBoxIdAfiliado.Text)))
+                String id = fila.Cells["id"].Value.ToString();
+
+                if (abm_usuario.get_plan_medico(id) == 0)
                 {
-                    if (abm_usuario.get_plan_medico(textBoxIdAfiliado.Text) == 0)
-                    {
-                        MessageBox.Show("El afiliado no posee un Plan Médico. Por favor, dirigirse al Menú Principal y seleccionar la opcion Plan Médico, luego comprar un Plan Médico.");
-                    }
-                    else
-                    {
-                        SeleccionarBono seleccionBono = new SeleccionarBono(textBoxIdAfiliado.Text, this);
-                        seleccionBono.Show();
-                        this.Hide();
-                    }
+                    MessageBox.Show("El afiliado no posee un Plan Médico. Por favor, dirigirse al Menú Principal y seleccionar la opcion Plan Médico, luego comprar un Plan Médico.");
                 }
                 else
-                    MessageBox.Show("El usuario no existe. Ingrese otro id");
+                {
+                    SeleccionarBono seleccionBono = new SeleccionarBono(id, this);
+                    seleccionBono.Show();
+                    this.Hide();
+                }
+
             }
-            else
-                MessageBox.Show("Por favor ingrese un id afiliado");
+            catch
+            {
+                MessageBox.Show("Seleccione una fila");
+            }
         }
     }
 }
